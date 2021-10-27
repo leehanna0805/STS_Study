26566d96d304086ddc34275258ed4bf3e16ab5b2
leehanna0805 <hannagood0805@gmail.com>
2021-10-27 10:59:36 +0900

    handler mapping 클래스

--- MVC_FW_Board/src/main/java/com/company/view/controller/HandlerMapping.java
+++ MVC_FW_Board/src/main/java/com/company/view/controller/HandlerMapping.java
@@ -1,5 +1,16 @@
 package com.company.view.controller;
 
-public class HandlerMapping {
+import java.util.HashMap;
+import java.util.Map;
 
+public class HandlerMapping {
+	private Map<String, Controller> mappings;
+	
+	//생성자
+	public HandlerMapping() {
+		//hashmap 객체생성. key는 string, 값은 controller
+		mappings = new HashMap<String, Controller>();
+		
+		
+	}
 }
