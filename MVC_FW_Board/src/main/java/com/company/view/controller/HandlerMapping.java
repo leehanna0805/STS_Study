c135e3131732c1c6f6e4435b79f0b4c3760a393c
leehanna0805 <hannagood0805@gmail.com>
2021-10-27 10:59:48 +0900

    Update HandlerMapping.java

--- MVC_FW_Board/src/main/java/com/company/view/controller/HandlerMapping.java
+++ MVC_FW_Board/src/main/java/com/company/view/controller/HandlerMapping.java
@@ -3,6 +3,14 @@ package com.company.view.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.company.view.board.DeleteBoardController;
+import com.company.view.board.GetBoardController;
+import com.company.view.board.GetBoardListController;
+import com.company.view.board.InsertBoardController;
+import com.company.view.board.UpdateBoardController;
+import com.company.view.user.LoginController;
+import com.company.view.user.LogoutController;
+
 public class HandlerMapping {
 	private Map<String, Controller> mappings;
 	
@@ -11,6 +19,18 @@ public class HandlerMapping {
 		//hashmap 객체생성. key는 string, 값은 controller
 		mappings = new HashMap<String, Controller>();
 		
-		
+		//action을 key로 PUT
+		mappings.put("/login.do", new LoginController());
+		mappings.put("/getBoardList", new GetBoardListController());
+		mappings.put("/getBoard.do", new GetBoardController());
+		mappings.put("/insertBoard.do", new InsertBoardController());
+		mappings.put("/updateBoard.do", new UpdateBoardController());
+		mappings.put("/deleteBoard.do", new DeleteBoardController());
+		mappings.put("/logout.do", new LogoutController());
+	}
+	
+	//사용자 정의 메소드 구현
+	public Controller getController(String path) {
+		return mappings.get(path);
 	}
 }
