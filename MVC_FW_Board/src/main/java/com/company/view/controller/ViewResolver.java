ab7343febe458a93805eb4aab4ebc3487e5b69fc
leehanna0805 <hannagood0805@gmail.com>
2021-10-27 11:37:40 +0900

    Update ViewResolver.java

--- MVC_FW_Board/src/main/java/com/company/view/controller/ViewResolver.java
+++ MVC_FW_Board/src/main/java/com/company/view/controller/ViewResolver.java
@@ -1,5 +1,23 @@
 package com.company.view.controller;
 
 public class ViewResolver {
-
+	//필드 선언
+	public String prefix; //접두사
+	public String suffix; //접미사
+	
+	public void setPrefix(String prefix) {
+		this.prefix = prefix;
+	}
+	public void setSuffix(String suffix) {
+		this.suffix = suffix;
+	}
+	
+	//사용자 정의 메소드
+	//ex)포워딩 할때 ==> view로 "getBoardList"넘어옴. 
+	//				prefix: "./" 
+	//				suffix: ".jsp" 
+	//	return "./getBoardList.jsp"
+	public String getView(String viewName) {
+		return prefix + viewName + suffix;
+	}
 }
