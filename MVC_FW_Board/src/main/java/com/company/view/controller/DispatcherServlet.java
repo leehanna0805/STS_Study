21dae758fe126740c461444e6245975c641dc77c
leehanna0805 <hannagood0805@gmail.com>
2021-10-27 11:44:44 +0900

    init()

--- MVC_FW_Board/src/main/java/com/company/view/controller/DispatcherServlet.java
+++ MVC_FW_Board/src/main/java/com/company/view/controller/DispatcherServlet.java
@@ -10,10 +10,20 @@ public class DispatcherServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
     
 	//객체 참조변수 선언
+	private HandlerMapping handlerMapping;
+	private ViewResolver viewResolver;
 	
+	//수동으로 메소드 오버라이드 시키는 방법: i 입력후 Ctrl+space
+	@Override
+	public void init() throws ServletException { //개발자가 아닌 서블릿컨테이너가 자동으로 호출함.
+		//멤버변수 초기화
+		handlerMapping = new HandlerMapping();
+		viewResolver = new ViewResolver();
+	}
 	
     public DispatcherServlet() {
         super();
+        
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
