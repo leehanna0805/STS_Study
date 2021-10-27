91938a10f27dfa892274e1ab52a3ff0acae54fdf
leehanna0805 <hannagood0805@gmail.com>
2021-10-27 11:37:27 +0900

    Update DispatcherServlet.java
    포워딩할때 return prefix+viewName+suffix

--- MVC_FW_Board/src/main/java/com/company/view/controller/DispatcherServlet.java
+++ MVC_FW_Board/src/main/java/com/company/view/controller/DispatcherServlet.java
@@ -6,34 +6,22 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class DispatcherServlet
- */
 public class DispatcherServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+    
+	//객체 참조변수 선언
+	
+	
     public DispatcherServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		
 	}
 
 }
