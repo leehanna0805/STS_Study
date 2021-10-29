e29ebcb9149a16fb85cb21a26ca6787cac6a8249
leehanna0805 <hannagood0805@gmail.com>
2021-10-29 10:05:55 +0900

    toString() 오버라이드

--- BusinessComponentEx/src/main/java/com/company/business/board/BoardDO.java
+++ BusinessComponentEx/src/main/java/com/company/business/board/BoardDO.java
@@ -1,5 +1,8 @@
 package com.company.business.board;
-
+/*
+ * 개발자가 구현하는 모든 클래스는 최상위 클래스인 Object클래스로부터 상속받는다.
+ * Object클래스의 모든 메소드를 BoardDO 자식클래스에서 @Override 할수있다능
+ */
 import java.sql.Date;
 
 public class BoardDO {
@@ -8,8 +11,6 @@ public class BoardDO {
 	private String writer;
 	private Date regdate;
 	private int cnt;
-	
-	
 	
 	//getter setter
 	public int getSeq() {return seq;}
@@ -22,6 +23,13 @@ public class BoardDO {
 	public void setRegdate(Date regdate) {this.regdate = regdate;}
 	public int getCnt() {return cnt;}
 	public void setCnt(int cnt) {this.cnt = cnt;}
+	
+	//Object클래스 메소드 오버라이드 하기
+	@Override
+	public String toString() {
+		return "BoardDO [seq=" + seq + ", title=" + title + ", writer=" + writer + ", regdate=" + regdate + ", cnt="
+				+ cnt + "]";
+	}
 	
 	
 }
