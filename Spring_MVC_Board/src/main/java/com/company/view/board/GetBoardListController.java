38b0a1e9f8657ff9f664fce5885c2bb9a19c8de4
leehanna0805 <hannagood0805@gmail.com>
2021-11-01 12:26:33 +0900

    Update GetBoardListController.java

--- Spring_MVC_Board/src/main/java/com/company/view/board/GetBoardListController.java
+++ Spring_MVC_Board/src/main/java/com/company/view/board/GetBoardListController.java
@@ -1,5 +1,39 @@
 package com.company.view.board;
 
-public class GetBoardListController {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import com.company.Spring_MVC_Board.board.BoardDAO;
+
+public class GetBoardListController implements Controller{
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		request.setCharacterEncoding("UTF-8");
+		System.out.println("전체게시글 목록 보기 처리");
+		
+		String searchField = "";
+		String searchText = "";
+		
+		if(request.getParameter("searchCondition") != ""
+				&& request.getParameter("searchKeyword") != "") {
+			searchField = request.getParameter("searchCondition");
+			searchText = request.getParameter("searchKeyword");
+		}
+		BoardDO boardDO = new BoardDO();
+		BoardDAO boardDAO = new BoardDAO();
+		List<BoardDO> boardList = boardDAO.getBoardList(searchField, searchText);
+		
+		//ModelAndView 객체 생성
+		ModelAndView mav = new ModelAndView();
+		
+		//포워딩 (세션에 등록하는 것이 아닌 mav에 등록)
+		mav.addObject("boardList", boardList);
+		
+		return mav;
+	}
 
 }
