30c3a08c8505a255c578661a9773a2ba16efe24f
leehanna0805 <hannagood0805@gmail.com>
2021-11-01 23:13:51 +0900

    나머지 crud controller 구현

--- Spring_MVC_Board/src/main/java/com/company/view/board/GetBoardListController.java
+++ Spring_MVC_Board/src/main/java/com/company/view/board/GetBoardListController.java
@@ -11,18 +11,19 @@ import org.springframework.web.servlet.mvc.Controller;
 import com.company.Spring_MVC_Board.board.BoardDAO;
 import com.company.Spring_MVC_Board.board.BoardDO;
 
-public class GetBoardListController implements Controller{
+public class GetBoardListController implements Controller {
 
 	@Override
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		request.setCharacterEncoding("UTF-8");
-		System.out.println("전체게시글 목록 보기 처리");
+		
+		System.out.println("전체 게시글 목록 보기 처리");
 		
 		String searchField = "";
 		String searchText = "";
 		
-		if(request.getParameter("searchCondition") != ""
-				&& request.getParameter("searchKeyword") != "") {
+		if (request.getParameter("searchCondition") != ""
+							&& request.getParameter("searchKeyword") != "") {
 			searchField = request.getParameter("searchCondition");
 			searchText = request.getParameter("searchKeyword");
 		}
@@ -30,15 +31,15 @@ public class GetBoardListController implements Controller{
 		BoardDAO boardDAO = new BoardDAO();
 		List<BoardDO> boardList = boardDAO.getBoardList(searchField, searchText);
 		
-		//ModelAndView 객체 생성
+		// ModelAndView 객체 생성
 		ModelAndView mav = new ModelAndView();
 		
-		//모델 정보 등록(세션에 등록하는 것이 아닌 mav에 등록)
-		mav.addObject("boardList", boardList);
+		mav.addObject("boardList", boardList);	// Model 정보 저장 즉 view에 전달할 값을 설정	
+		// 포워딩
+		//		/WEB-INF/board/getBoardList.jsp
+		mav.setViewName("getBoardList");	// View 정보 저장 즉 응답할 view 이름 설정
 		
-		//포워딩
-		mav.setViewName("getBoardList");
 		return mav;
 	}
-
+	
 }
--- Spring_MVC_Board/src/main/java/com/company/view/user/LoginController.java
+++ Spring_MVC_Board/src/main/java/com/company/view/user/LoginController.java
@@ -1,3 +1,4 @@
+
 package com.company.view.user;
 
 import javax.servlet.http.HttpServletRequest;
@@ -10,16 +11,12 @@ import org.springframework.web.servlet.mvc.Controller;
 import com.company.Spring_MVC_Board.user.UserDAO;
 import com.company.Spring_MVC_Board.user.UserDO;
 
-/*
- * MVC패턴에선 Controller 인터페이스를 만들어서 implements했었는데
- * 스프링에선 Controller 인터페이스를 지원해줌. 구현할 필요 X
- */
 public class LoginController implements Controller{
 
-	/* 리턴 타입이 ModelAndView */
-	@Override
+	@Override	// 재정의
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		System.out.println("로그인 처리");
+		
 		String id = request.getParameter("id");
 		String password = request.getParameter("password");
 		
@@ -30,24 +27,24 @@ public class LoginController implements Controller{
 		UserDAO userDAO = new UserDAO();
 		UserDO user = userDAO.getUser(userDO);
 		
-		//ModelAndView 객체생성
+		// ModelAndView 객체 생성 
 		ModelAndView mav = new ModelAndView();
 		
-		if(user!=null) {
+		if (user != null) {
 			HttpSession session = request.getSession();
-			session.setAttribute("IdKey",id);
+			session.setAttribute("IdKey", id);
 			
 			System.out.println("로그인 인증 성공");
-			//포워딩
+			// 포워딩
 			mav.setViewName("redirect:getBoardList.do");
-			
-		}else {
-			System.out.println("로그인 실패");
-			//포워딩
+		} else {
+			System.out.println("로그인 인증 실패");
 			mav.setViewName("redirect:login.jsp");
 		}
-		return mav;
 		
+		return mav;
 	}
 
 }
+
+
