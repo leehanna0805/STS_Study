d03a7f26ed358baffa9e8d9c1ec84830578a493f
leehanna0805 <hannagood0805@gmail.com>
2021-11-02 14:15:13 +0900

    insertUser() 추가

--- Spring_Annotation_Board/src/main/java/com/company/annotation/user/UserDAO.java
+++ Spring_Annotation_Board/src/main/java/com/company/annotation/user/UserDAO.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 import com.company.annotation.common.JDBCUtil;
+import com.company.annotation.common.PasswordEncryptUtil;
 
 public class UserDAO {
 	
@@ -19,8 +20,29 @@ public class UserDAO {
 		String pwEncrypt;
 		private final String USERS_INSERT = "insert into users values(?,?,?,?,?)"; //회원가입 SQL
 		
-		
-		
+		public void insertUser(UserDO userDO){
+			try {
+				conn = JDBCUtil.getConnection();
+				pstmt = conn.prepareStatement(USERS_INSERT);
+				pstmt.setString(1, userDO.getId());
+				pstmt.setString(2, userDO.getPassword());
+				
+				//넘어온 패스워드를 암호화 시켜서 세번쨰 물음표 값으로 지정한다.
+				String plainText = userDO.getPassword();
+				pwEncrypt = PasswordEncryptUtil.encryptSHA256(plainText);
+				pstmt.setString(3, pwEncrypt);
+				
+				pstmt.setString(4, userDO.getName());
+				pstmt.setString(5, userDO.getRole());
+				
+				pstmt.executeUpdate();
+				
+			}catch(Exception e) {
+				e.printStackTrace();
+			}finally {
+				JDBCUtil.close(pstmt, conn);
+			}
+		}
 		
 		
 		public UserDO getUser(UserDO userObj) {	
